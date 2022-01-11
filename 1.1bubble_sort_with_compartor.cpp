3a849aacef7388e4360042c5ea5b318438e36193
Harshita0201 <62836828+Harshita0201@users.noreply.github.com>
2022-01-11 17:44:42 +0530

    Create 1.1bubble_sort_with_compartor.cpp

---
+++ 1.1bubble_sort_with_compartor.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+void print(int a[], int size){
+    for(int i=0; i<size; i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+bool compare(int a, int b){
+    return a>b;
+}
+
+void bubble_sort(int a[], int n, bool (&cmp)(int x, int y)){
+    for(int i=1; i<=(n-1); i++){
+        for(int j=0; j<=(n-i-1); j++){
+            if(cmp(a[j], a[j+1])){
+                swap(a[j],a[j+1]);
+            }
+        }
+    }
+    print(a,n);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin >>  a[i];
+    }
+    
+    bubble_sort(a,n,compare);
+    
+    return 0;
+}
