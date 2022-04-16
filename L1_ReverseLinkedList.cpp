6432efb77bc6b640641809e216b54827da5832ee
Harshita Sharma <62836828+Harshita0201@users.noreply.github.com>
2022-04-16 10:53:18 +0530

    Update L1_ReverseLinkedList.cpp

--- L1_ReverseLinkedList.cpp
+++ L1_ReverseLinkedList.cpp
@@ -33,3 +33,58 @@ public:
 		return prev;
     }
 };
+
+
+
+//recursive approach 1
+
+// class Solution {
+// public:
+//     void reverse(ListNode* &head, ListNode* curr, ListNode* prev){
+//         //base case
+//         //when curr reached the end of list
+//         if(curr==NULL){
+//             head=prev;
+//             return;
+//         }
+        
+//         ListNode *forward=curr->next;
+//         reverse(head, forward, curr);
+//         curr->next=prev;
+//     }
+//     ListNode* reverseList(ListNode* head) {
+//         //rercursive
+//         ListNode *prev=NULL;
+//         ListNode *curr=head;
+//         reverse(head, curr, prev);
+//         return head;
+        
+        
+//     }
+// };
+
+
+//recursive appraoch 2
+// class Solution {
+// public:
+
+    
+//     ListNode* reverse(ListNode *head){ //reverse 2
+//         //base case
+//         if(head==NULL || head->next==NULL){
+//             return head;
+//         }
+        
+//         ListNode *c_head= reverse(head->next);
+//         head->next->next=head;
+//         head->next=NULL;
+//         return c_head;
+            
+//     }
+//     ListNode* reverseList(ListNode* head) {
+        
+//         return reverse(head);
+      
+//     }
+// };
+	
