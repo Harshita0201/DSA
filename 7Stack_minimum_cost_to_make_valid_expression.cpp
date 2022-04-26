bbf7a84a977ed682f852f44de652f09f2955146b
Harshita Sharma <62836828+Harshita0201@users.noreply.github.com>
2022-04-26 18:39:06 +0530

    Create 7Stack_minimum_cost_to_make_valid_expression.cpp

---
+++ 7Stack_minimum_cost_to_make_valid_expression.cpp
@@ -0,0 +1,50 @@
+// Ninja has been given a string ‘STR’ containing either ‘{’ or ‘}’. 'STR’ is called valid if all the brackets are balanced. Formally for each opening bracket, there must be a closing bracket right to it.
+// For Example:
+// “{}{}”, “{{}}”, “{{}{}}” are valid strings while “}{}”, “{}}{{}”, “{{}}}{“ are not valid strings.
+
+
+#include<stack>
+int findMinimumCost(string str) {
+  	
+    //odd condition
+    if(str.length()%2 == 1) {
+        return -1;
+    }
+    
+    stack<char> s;
+    for(int i=0; i<str.length(); i++) {
+        char ch = str[i];
+        
+        if(ch == '{') 
+            s.push(ch);
+		else
+        {
+            //ch is closed brace
+            if(!s.empty() && s.top() == '{') {
+                s.pop();
+            }
+            else
+            {
+                s.push(ch);
+            }
+        }
+    }
+    
+        //stack contains invalid expression
+        int a = 0, b = 0;
+        while(!s.empty()) {
+            if(s.top() == '{') {
+                b++;
+            }
+            else
+            {
+                a++;
+            }
+            s.pop();
+        }
+        
+		int ans = (a+1)/2 + (b+1)/2;
+        return ans;
+    
+    
+}
