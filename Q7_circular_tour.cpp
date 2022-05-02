ba5b4b46d7c959115f1d013550232ab507d2fcc2
Harshita Sharma <62836828+Harshita0201@users.noreply.github.com>
2022-05-02 12:10:29 +0530

    Create Q7_circular_tour.cpp

---
+++ Q7_circular_tour.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};
+
+
+ // } Driver Code Ends
+/*
+The structure of petrolPump is 
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};*/
+
+/*You are required to complete this method*/
+class Solution{
+  public:
+  
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+    int tour(petrolPump p[],int n)
+    {
+       //Your code here
+       
+       int deficit = 0;
+       int balance = 0;
+       int start = 0;
+       
+       for(int i=0; i<n; i++) {
+           balance += p[i].petrol - p[i].distance;
+           if(balance < 0) {
+               start = i+1;//reset start
+               deficit += balance;
+               balance = 0; //reset balance
+           }
+       }
+       
+       if(balance + deficit >=0) 
+            return start;
+        return -1;
+       
+    }
+    
+};
