bac89e39fa6890ab06c98a5742a90e1cad86547a
Harshita Sharma <62836828+Harshita0201@users.noreply.github.com>
2022-06-03 17:42:42 +0530

    Create heap_3_kth_Smallest_Element.cpp

---
+++ heap_3_kth_Smallest_Element.cpp
@@ -0,0 +1,25 @@
+// arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        priority_queue<int> pq;
+        
+        //step 1: create max heap of first k elements 
+        for(int i=0; i<k; i++){
+            pq.push(arr[i]);
+        }
+        
+        //step 2: for rest kth to n-1th index 
+        for(int i=k; i<=r; i++){
+            if(arr[i]<pq.top()){
+                pq.pop();
+                pq.push(arr[i]);
+            }
+        }
+        
+        //step 3: ans is pq.top
+        int ans= pq.top();
+        return ans;
+    }
