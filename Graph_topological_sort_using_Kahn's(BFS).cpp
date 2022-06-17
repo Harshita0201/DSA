1d99ec5fd1e470faad310b883ff9e717112ca374
Harshita Sharma <62836828+Harshita0201@users.noreply.github.com>
2022-06-17 22:25:08 +0530

    Create Graph_topological_sort_using_Kahn's(BFS).cpp

---
+++ Graph_topological_sort_using_Kahn's(BFS).cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    
+    //create adjacency list 
+    unordered_map<int, list<int>> adj;
+    for(int i=0; i<e; i++){
+        int u=edges[i][0];
+        int v=edges[i][1];
+        
+        //directed graph
+        adj[u].push_back(v);
+    }
+    
+    //stroe indegree of all vertexes
+    vector<int> indegree(v);
+    for(auto i:adj){
+        for(auto j:i.second){
+            indegree[j]++;
+        }
+    }
+    
+    //push all 0 indegree nodes into queue
+    queue<int> q;
+    for(int i=0; i<v; i++){
+        if(indegree[i]==0){
+            q.push(i);
+        }
+    }
+    
+    //do bfs
+    vector<int> ans;
+    while(!q.empty()){
+        int front=q.front();
+        q.pop();
+        
+        //store into ans
+        ans.push_back(front);
+        
+        //for all the neighbours of front
+        for(auto neighbour:adj[front]){
+            indegree[neighbour]--; //reduce the indegree as its parent node is popped
+            if(indegree[neighbour]==0){
+                q.push(neighbour);
+            }
+        }
+    }
+    return ans;
+}
