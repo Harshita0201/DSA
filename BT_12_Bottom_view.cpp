6d128d6d71ee15086b7056eb6f8b1b6d0c22f6d7
Harshita Sharma <62836828+Harshita0201@users.noreply.github.com>
2022-07-24 11:14:55 +0530

    Update BT_12_Bottom_view.cpp

--- BT_12_Bottom_view.cpp
+++ BT_12_Bottom_view.cpp
@@ -1,39 +1,37 @@
-  vector<int> ans;
-        if(root==NULL){
-            return ans;
-        }
-
-        map<int, int> m; //mapping of HD and node->data
-        queue<pair<Node*, int>> q; //mapping of node and coresponding HD
-
-        //push the root node and its HD for root it is 0
-        q.push(make_pair(root,0));
-
-        while(!q.empty()){
-            pair<Node*, int> temp=q.front();
-            q.pop();
-
-            Node * frontNode=temp.first;
-            int hd=temp.second;
-
-
-            //for 1 to 1 mapping of HD and node
-            //if a node is present corresponding to a HD then do nothing
-            if(m.find(hd)==m.end()){ //if not present
-                m[hd]=frontNode->data;
-            }
-
-            if(frontNode->left){
-                q.push(make_pair(frontNode->left, hd-1));
-            }
-            if(frontNode->right){
-                q.push(make_pair(frontNode->right, hd+1));
-            }
-
-        }
-
-        for(auto i:m){
-            ans.push_back(i.second); 
-        }
-
-        return ans;
+class Solution {
+  public:
+    vector <int> bottomView(Node *root) {
+       vector<int> ans;
+       
+       if(root==NULL){
+           return ans;
+       }
+       
+       map<int, int> m; //mapping of hd, and node->data
+       queue<pair<Node *, int>> q; //node and hd
+       
+       q.push({root, 0}); //root at hd 0
+       
+       while(!q.empty()){
+           pair<Node *, int> temp=q.front();
+           q.pop();
+           
+           int hd=temp.second;
+           Node* node=temp.first;
+           
+           m[hd]=node->data;//stroe node at hd in map
+           
+           if(node->left){
+               q.push({node->left, hd-1});
+           }
+           if(node->right){
+               q.push({node->right, hd+1});
+           }
+       }
+       
+       for(auto i:m){
+           ans.push_back(i.second);
+       }
+       return ans;
+    }
+};
