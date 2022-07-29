c38ae8e9d53314fe78a44a5b6debca1bab794585
Harshita Sharma <62836828+Harshita0201@users.noreply.github.com>
2022-07-29 18:39:39 +0530

    Update maximum_sum_of_non_adjacent_elements.cpp

--- DP/maximum_sum_of_non_adjacent_elements.cpp
+++ DP/maximum_sum_of_non_adjacent_elements.cpp
@@ -1,9 +1,11 @@
+//https://www.codingninjas.com/codestudio/problems/maximum-sum-of-non-adjacent-elements_843261?leftPanelTab=0&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbar
+
 int solve(vector<int> &nums, int ind){
-    if(ind<0){ //no element left 
+    if(ind<0){
         return 0;
     }
     
-    if(ind==0){//reached 0 index element or one element
+    if(ind==0){//reached 0 index element
         return nums[0]; 
     }
     
@@ -33,13 +35,54 @@ int solve2(vector<int> &nums, int ind, vector<int> &dp){
     dp[ind]=max(inc, exc);
     return dp[ind];
 }
+
+int solve3(vector<int> &nums){
+    int n=nums.size();
+    vector<int> dp(n, 0);
+    
+    //bottom up
+    //base case
+    dp[0]=nums[0];
+    
+    //for rest
+    for(int i=1; i<n; i++){
+        int inc= nums[i] + dp[i-2];
+        int exc= 0 + dp[i-1];
+        dp[i]= max(inc, exc);
+    }
+    return dp[n-1];
+}
+int solve4(vector<int> &nums){
+    int n=nums.size();
+    
+    int prev2=0;
+    int prev1=nums[0];
+    
+    for(int i=1; i<n; i++){
+        int inc= nums[i] + prev2;
+        int exc= 0 + prev1;
+        int ans=max(prev1, prev2);
+        
+        //move ahead
+        prev2 = prev1;
+        prev1 = ans; 
+    }
+    
+    return prev1;
+}
 int maximumNonAdjacentSum(vector<int> &nums){
     int n = nums.size();
     //recursive soln 
     //int ans = solve(nums, n-1); //starting from the end
     //return ans;
     
-    //dp soln
+    //dp soln memoization tc:o(n) +o(n) sc:o(n)
     vector<int> dp(n, -1); //initially -1
-    return solve2(nums, n-1, dp);
+    //return solve2(nums, n-1, dp);
+    
+    //dp tabulation tc:o(n) sc:o(n)
+    return solve3(nums);
+    
+    //space optimized tc:o(n) sc:o(1)
+    return solve4(nums);
 }
